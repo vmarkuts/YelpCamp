2cd2d77c566826624a5f1a411748fc05439c8acc
vlad <vlad.markuts@gmail.com>
2020-04-20 23:22:43 +0300

    adding header, footer, bootstrap

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var faker = require('faker');
 
 app.set('view engine', 'ejs');
 
@@ -8,7 +9,13 @@ app.get('/', function(req, res) {
 });
 
 app.get('/campgrounds', function(req, res) {
-	
+	var campgrounds = [
+		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()},
+		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()},
+		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()}
+	]
+
+	res.render('campgrounds', {campgrounds: campgrounds});
 })
 
 app.listen(4000, function(){
