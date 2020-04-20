d4f6844e0d4b491b0417ccb30f0647a827f5bbbe
vlad <vlad.markuts@gmail.com>
2020-04-20 23:38:07 +0300

    making form to create a campground

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 var express = require('express');
 var app = express();
 var faker = require('faker');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+var campgrounds = [
+		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()},
+		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()},
+		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()}
+	];
 
 app.get('/', function(req, res) {
 	res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-	var campgrounds = [
-		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()},
-		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()},
-		{name: faker.address.city()+', '+faker.address.county(), image: faker.image.avatar()}
-	]
-
 	res.render('campgrounds', {campgrounds: campgrounds});
-})
+});
+
+app.post('/campgrounds', function(req, res) {
+	//get data from form and add to array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	//redirect back to home page
+	res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res){
+	res.render('new')
+});
 
 app.listen(4000, function(){
 	console.log('The YelpCamp Server Has Started');
