641941f88273bd1091ade76ae9b448d75ab6b2d1
vlad <vlad.markuts@gmail.com>
2020-04-20 21:41:45 +0300

    structure

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
+	
+})
+
 app.listen(4000, function(){
 	console.log('The YelpCamp Server Has Started');
 })
