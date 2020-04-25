ec890cb5f765bfa849c15f10b4b23634325a9baf
vlad <vlad.markuts@gmail.com>
2020-04-25 23:42:18 +0300

    adding page for each camp

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.set('view engine', 'ejs');
 //shema setup
 var campgroundSchema = new mongoose.Schema({
 		name: String,
-		image: String
+		image: String,
+		description: String
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
@@ -26,7 +27,7 @@ app.get('/campgrounds', function(req, res) {
 		if (err) {
 			console.log(err);
 		} else {
-			res.render('campgrounds',{campgrounds:allCampground})
+			res.render('index',{campgrounds:allCampground})
 		}
 	})
 });
@@ -35,7 +36,8 @@ app.post('/campgrounds', function(req, res) {
 	//get data from form and add to array
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};
 	//create new campground and save to db
 	Campground.create(newCampground, function(err,newlyCreated){
 		if (err) {
@@ -46,9 +48,33 @@ app.post('/campgrounds', function(req, res) {
 	});
 });
 
+// Campground.create({
+// 	name: faker.fake('{{address.city}}, {{address.county}}'),
+// 	image: faker.image.image(),
+// 	description: faker.company.catchPhraseDescriptor()
+// }, function(err,campground){
+// 	if (err) {
+// 	console.log(err);
+// } else {
+// 	console.log('new camp ' + campground);
+// }
+// });
+
 app.get('/campgrounds/new', function(req, res){
 	res.render('new')
 });
+
+app.get('/campgrounds/:id', function(req, res){
+	//find requested id
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if (err) {
+			console.log(err);
+		} else {
+			//rendeer show
+			res.render('show', {campground: foundCampground});
+		}
+	});
+	})
 
 app.listen(4001, function(){
 	console.log('The YelpCamp Server Has Started');
